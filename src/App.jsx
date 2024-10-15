f3b3ba1b364f971fdbcb571ab4d942212b630680
fergone03 <114108805+fergone03@users.noreply.github.com>
2024-10-16 00:34:03 +0200

    1

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import HomePage from "./HomePage";
 import BlackJack from "./BlackJack";
 import Ruleta from "./Ruleta";
 import AllGames from "./AllGames";
-import "./styles.css"; // Importa los estilos
+import "./styles.css"; 
 import logo from "./assets/logo.svg"
 
 
@@ -13,25 +13,17 @@ const App = () => {
   return (
     <Router>
       <header>
+             <div class="logo">
               <img src={logo} alt="Custom Logo" style={{ height: '60px' }} />
+              <h2 className="nombre-empresa">FortuneFlare</h2>
+              </div> 
         <nav>
-          <ul>
-            <li>
-              <Link to="/">Homepage</Link>
-            </li>
-            <li>
-              <Link to="/slots">Slots</Link>
-            </li>
-            <li>
-              <Link to="/blackjack">BlackJack</Link>
-            </li>
-            <li>
-              <Link to="/ruleta">Ruleta</Link>
-            </li>
-            <li>
-              <Link to="/allgames">Todos los juegos</Link>
-            </li>
-          </ul>
+       <a href="./HomePage.jsx" className="navlink">Homepage</a>
+       <a href="./SlotMachine.jsx" className="navlink">Slots</a>
+       <a href="./BlackJack.jsx" className="navlink">BlackJack</a>
+       <a href="./Ruleta.jsx" className="navlink">Ruleta</a>
+       <a href="./AllGames.jsx" className="navlink">Todos los juegos</a>
+
         </nav>
       </header>
 
